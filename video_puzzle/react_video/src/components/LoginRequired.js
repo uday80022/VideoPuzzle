86529da83be5fd53e94a9cde2cffa17fbcb4d5fe
Rajeev <rajeevmullapati@gmail.com>
2023-12-09 12:51:48 +0530

    login changes

--- video_puzzle/react_video/src/components/LoginRequired.js
+++ video_puzzle/react_video/src/components/LoginRequired.js
@@ -10,7 +10,7 @@ const LoginRequired = () => {
 
   return (
     <div>
-      <p>Please login and register</p>
+      <p>Please login or register</p>
       <button onClick={redirectToLogin}>Go to Login</button>
     </div>
   );
